4c9c5e578f463ca793c4bc0d533f78a06b7b0c0b
Vignesh <dhinesh139ad@gmail.com>
2023-06-02 21:50:44 +0530

    notes update is added successfully

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -45,4 +45,39 @@ router.post(
     }
   }
 );
+
+// Route - 3 => the below code is to update the existing notes
+router.put("/update/:id", fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
+
+  // create a newNote object
+  const newNote = {};
+  // if value exist in the fields then the value is added to newNote
+  if (title) {
+    newNote.title = title;
+  }
+  if (description) {
+    newNote.description = description;
+  }
+  if (tag) {
+    newNote.tag = tag;
+  }
+  // find the note to be updated and update it
+  let note = await Notes.findById(req.params.id);
+
+  // if note does not exist in that id
+  if (!note) {
+    return res.status(404).send("Notes does not exits");
+  }
+
+  if (note.user.toString() !== req.user.id) {
+    return res.status(401).send("Not allowed");
+  }
+  note = await Notes.findByIdAndUpdate(
+    req.params.id,
+    { $set: newNote },
+    { new: true }
+  );
+  res.json({ note });
+});
 module.exports = router;
