16ea7a12083e59f2c44006f9471fd2bdc59265c9
Vignesh <dhinesh139ad@gmail.com>
2023-06-07 11:57:08 +0530

    Context API is added successfully to the project

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import Navbar from "./Components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import About from "./Components/About";
 import Home from "./Components/Home";
+import NoteState from "./Context/NoteState";
 
 function App() {
   return (
-    <>
+    <NoteState>
       <Navbar />
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </>
+    </NoteState>
   );
 }
 
