2fad74c769124efa270cb099a992ea8064f2b537
Vignesh <dhinesh139ad@gmail.com>
2023-06-07 12:01:36 +0530

    state constant in NoteState.j file is removed and UseContext in About.js file is also removed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          Navbar
+          Notebook
         </Link>
         <button
           className="navbar-toggler"
--- src/Context/NoteState.js
+++ src/Context/NoteState.js
@@ -2,11 +2,9 @@ import React from "react";
 import NoteContext from "./NoteContext";
 
 const NoteState = (props) => {
-  const state = {
-    name: "vignesh",
-  };
+ 
   return (
-    <NoteContext.Provider value={state}>{props.children}</NoteContext.Provider>
+    <NoteContext.Provider value={{}}>{props.children}</NoteContext.Provider>
   );
 };
 
